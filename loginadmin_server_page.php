3ae00819d4ebfbb19ba91e93a4fb8534d2637a78
navyaungarala349@gmail.com <navya>
2024-12-09 17:57:35 +0530

    first commit

---
+++ loginadmin_server_page.php
@@ -0,0 +1,31 @@
+<?php
+
+include("data_class.php");
+
+$login_email=$_GET['login_email'];
+$login_pasword=$_GET['login_pasword'];
+
+if($login_email==null||$login_pasword==null){
+    $emailmsg="";
+    $pasdmsg="";
+    
+    if($login_email==null){
+        $emailmsg="Enter your email";
+    }
+    if($login_pasword==null){
+        $pasdmsg="Enter your password";
+    }
+
+    header("Location: index.php?ademailmsg=$emailmsg&adpasdmsg=$pasdmsg");
+}
+
+elseif($login_email!=null&&$login_pasword!=null){
+    $obj=new data();
+    $obj->setconnection();
+    $obj->adminLogin($login_email,$login_pasword);
+
+}
+
+
+
+
